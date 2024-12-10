0ba2a59041e4dc14582b227974496e25a1d4ecd1
nel-data07 <nelly.ariib@gmail.com>
2024-12-10 01:08:35 +0100

    modif partie bi-variée

--- FIFI_Nelly_1_dashboard_122024.py
+++ FIFI_Nelly_1_dashboard_122024.py
@@ -307,9 +307,9 @@ if selected == "Analyse Bi-Variée":
                             palette='viridis',
                             ax=ax
                         )
-                        ax.set_title(f'Analyse bi-variée: {feature_x} vs {feature_y}', fontsize=10)
-                        ax.set_xlabel(feature_x, fontsize=8)
-                        ax.set_ylabel(feature_y, fontsize=8)
+                        ax.set_title(f'Analyse bi-variée: {feature_x} vs {feature_y}', fontsize=6)
+                        ax.set_xlabel(feature_x, fontsize=6)
+                        ax.set_ylabel(feature_y, fontsize=6)
                         
                         st.pyplot(fig)
                         st.caption(
